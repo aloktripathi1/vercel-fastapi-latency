d82492ad5d09227e6b89d872a7130ff530388e30
Alok Tripathi <142301030+aloktripathi1@users.noreply.github.com>
2026-01-08 10:37:27 +0000

    fix cors

--- api/index.py
+++ api/index.py
@@ -7,8 +7,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
-    allow_methods=["POST"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
